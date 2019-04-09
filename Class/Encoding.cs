5112013d7c04721ec05fb0c9419d530e53f38d26
CarlosMarioSom <krlosmario2805@hotmail.com>
2019-04-09 13:16:28 -0600

    Estructira base de codigo, falta codificacion

--- Class/Encoding.cs
+++ Class/Encoding.cs
@@ -17,21 +17,41 @@ public class Encoding
     public string encodingFile()
     {
         string message = "";
-        string binaryOfFile = "";
+        string codedFile = "";
+        string groupOf3Bytes = "";
 
         // Reading file by bytes
         byte[] fileBytes = File.ReadAllBytes(Argumentos.Entrada);
         StringBuilder stringBuilder = new StringBuilder();
 
+        int byteNumber = 1;
         foreach (byte b in fileBytes)
         {
             stringBuilder.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+            if (byteNumber % 3 == 0)
+            {
+                groupOf3Bytes = stringBuilder.ToString();
+                codedFile += this.encode3Bytes(groupOf3Bytes);
+                stringBuilder.Clear();
+                Console.WriteLine(groupOf3Bytes);
+            }
+            byteNumber++;
         }
-        binaryOfFile = stringBuilder.ToString();
+        codedFile += this.encodeIncompleteBytes(stringBuilder);
+        Console.WriteLine(stringBuilder.ToString());
 
-        //Console.WriteLine(binaryOfFile);
-
-        message = binaryOfFile;
         return message;
+    }
+
+    private string encode3Bytes(string groupOf3Bytes)
+    {
+        string encoded = "";
+        return encoded;
+    }
+
+    private string encodeIncompleteBytes(StringBuilder stringBuilder)
+    {
+        string encoded = "";
+        return encoded;
     }
 }
